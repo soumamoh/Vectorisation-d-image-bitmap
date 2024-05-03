f46bae799e58acf7dd21b657e51356ae22b4093d
soumamoh <98470702+soumamoh@users.noreply.github.com>
2024-05-03 16:09:29 +0200

    Update test_sequence.c

--- test_sequence.c
+++ test_sequence.c
@@ -6,7 +6,7 @@ int main () {
 
 	Liste_Liste_Point LL = creer_liste_liste_Point_vide ();
 	double x,y;
-	for ( int i = 0; i<3;i++) {
+	for (int i = 0; i<3;i++) {
 		Liste_Point L = creer_liste_Point_vide ();
 		printf("x = ");
 		scanf("%lf",&x);
